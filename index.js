979faa954b7011516d78897fa1f6416b6f31b5c5
cathy <oonxt@msn.com>
2017-09-06 21:22:27 +0800

    update bluebird

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-/**
- * Created by lvbingru on 1/5/16.
- */
+import { NativeAppEventEmitter, NativeModules } from 'react-native';
+// import promisify from 'es6-promisify';
+import Promise from 'bluebird'
 
-import {NativeModules, NativeAppEventEmitter} from 'react-native';
-import Promise from 'bulebird';
+const WeChatAPI = NativeModules.WeChatAPI;
 
-const {WeiboAPI} = NativeModules;
-
-// Used only with promisify. Transform callback to promise result.
 function translateError(err, result) {
     if (!err) {
         return this.resolve(result);
@@ -23,67 +19,98 @@ function translateError(err, result) {
     this.reject(Object.assign(new Error(), { origin: err }));
 }
 
-function wrapApi(nativeFunc) {
+// Save callback and wait for future event.
+let savedCallback = undefined;
+function waitForResponse(type) {
+    return new Promise((resolve, reject) => {
+        if (savedCallback) {
+            savedCallback('User canceled.');
+        }
+        savedCallback = result => {
+            if (result.type !== type) {
+                //
+                //if (__DEV__) {
+                //  throw new Error('Unsupported response type: ' + resp.type);
+                //}
+                return;
+            }
+            savedCallback = undefined;
+            if (result.errCode !== 0) {
+                const err = new Error(result.errMsg);
+                err.errCode = result.errCode;
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        };
+    });
+}
+
+NativeAppEventEmitter.addListener('WeChat_Resp', resp => {
+    const callback = savedCallback;
+    savedCallback = undefined;
+    callback && callback(resp);
+});
+
+
+function wrapCheckApi(nativeFunc) {
     if (!nativeFunc) {
         return undefined;
     }
+
     const promisified = Promise.promisify(nativeFunc, translateError);
     return (...args) => {
         return promisified(...args);
     };
 }
 
-// Save callback and wait for future event.
-let savedCallback = undefined;
-function waitForResponse(type) {
-    return new Promise((resolve, reject) => {
-            if (savedCallback) {
-                savedCallback('User canceled.');
-            }
-            savedCallback = result => {
-            if (result.type !== type) {
-        return;
+export const isWXAppInstalled = wrapCheckApi(WeChatAPI.isWXAppInstalled);
+export const isWXAppSupportApi = wrapCheckApi(WeChatAPI.isWXAppSupportApi);
+
+function wrapApi(nativeFunc) {
+    if (!nativeFunc) {
+        return undefined;
     }
-    savedCallback = undefined;
-    if (result.errCode !== 0) {
-        const err = new Error(result.errMsg);
-        err.errCode = result.errCode;
-        reject(err);
-    } else {
-        resolve(result);
-    }
-};
-});
+
+    const promisified = Promise.promisify(nativeFunc, translateError);
+    return async function (...args) {
+        if (!WeChatAPI.isAppRegistered) {
+            throw new Error('注册应用失败');
+        }
+        const checkInstalled = await isWXAppInstalled();
+        if (!checkInstalled) {
+            throw new Error('没有安装微信!');
+        }
+        const checkSupport = await isWXAppSupportApi();
+        if (!checkSupport) {
+            throw new Error('微信版本不支持');
+        }
+        return await promisified(...args);
+    };
 }
 
-NativeAppEventEmitter.addListener('Weibo_Resp', resp => {
-    const callback = savedCallback;
-savedCallback = undefined;
-callback && callback(resp);
-});
+const nativeSendAuthRequest = wrapApi(WeChatAPI.login);
+const nativeShareToTimelineRequest = wrapApi(WeChatAPI.shareToTimeline);
+const nativeShareToSessionRequest = wrapApi(WeChatAPI.shareToSession);
+const nativePayRequest = wrapApi(WeChatAPI.pay);
 
-
-const defaultScope = "all"
-const defaultRedirectURI = "https://api.weibo.com/oauth2/default.html"
-
-function checkData(data) {
-    if(!data.redirectURI) {
-        data.redirectURI = defaultRedirectURI
-    }
-    if(!data.scope) {
-        data.scope = defaultScope
-    }
+export function login(config) {
+    const scope = (config && config.scope) || 'snsapi_userinfo';
+    return nativeSendAuthRequest({scope})
+        .then(() => waitForResponse("SendAuth.Resp"));
 }
 
-const nativeSendAuthRequest = wrapApi(WeiboAPI.login);
-const nativeSendMessageRequest = wrapApi(WeiboAPI.shareToWeibo);
-
-export function login(config={}) {
-    checkData(config)
-    return Promise.all([waitForResponse('WBAuthorizeResponse'), nativeSendAuthRequest(config)]).then(v=>v[0]);
+export function shareToTimeline(data) {
+    return nativeShareToTimelineRequest(data)
+        .then(() => waitForResponse("SendMessageToWX.Resp"));
 }
 
-export function share(data) {
-    checkData(data)
-    return Promise.all([waitForResponse('WBSendMessageToWeiboResponse'), nativeSendMessageRequest(data)]).then(v=>v[0]);
+export function shareToSession(data) {
+    return nativeShareToSessionRequest(data)
+        .then(() => waitForResponse("SendMessageToWX.Resp"));
+}
+
+export function pay(data) {
+    return nativePayRequest(data)
+        .then(() => waitForResponse("Pay.Resp"));
 }
